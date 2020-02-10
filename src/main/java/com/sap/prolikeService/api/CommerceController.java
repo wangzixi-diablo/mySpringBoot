d97edecc4c74dddf4fbb17c214dca3911ed39699
i042416 <jerry.wang@sap.com>
2020-02-10 10:32:04 +0800

    learn how to set log level at root level

--- src/main/java/com/sap/prolikeService/api/CommerceController.java
+++ src/main/java/com/sap/prolikeService/api/CommerceController.java
@@ -1,5 +1,8 @@
 package com.sap.prolikeService.api;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +13,19 @@ public class CommerceController {
 	
     private static final Logger log = LoggerFactory.getLogger(CommerceController.class);
 	
+    private String getCurrentDate() {
+    	SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+    	Date date = new Date(System.currentTimeMillis());
+    	return formatter.format(date);
+    }
+    
 	@GetMapping("/product")
     public String getProductDetail() {
+		final Logger log2 = LoggerFactory.getLogger(CommerceController.class);
+		String logInfo = "Hello world method called:" + getCurrentDate();
+		System.out.println(logInfo);
+		log2.info(logInfo);
+		log.info(logInfo);
 	    return "Hello World";
     }
 }
